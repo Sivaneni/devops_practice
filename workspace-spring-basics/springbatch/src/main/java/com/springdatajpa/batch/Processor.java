c10fe5eaa914c0d6fb3f5fefbea7d3add8b42caa
Sivaneni <sivaneni1992@gmail.com>
2022-12-27 15:47:54 +0530

    adding demo-2

---
+++ workspace-spring-basics/springbatch/src/main/java/com/springdatajpa/batch/Processor.java
@@ -0,0 +1,19 @@
+package com.springdatajpa.batch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class Processor implements ItemProcessor<String, String> {
+
+	@Override
+	public O process(I item) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String process(String item) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
